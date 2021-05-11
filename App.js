0fc4696a8c880eafd73b50111065cc37c5f0ffdb
Hetali-Shah <hetali1311.shah@gmail.com>
2021-05-11 18:58:52 +0530

    Plot chart

--- App.js
+++ App.js
@@ -1,49 +1,84 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import Svg, {
+  Circle,
+  Ellipse,
+  G,
+  Text,
+  TSpan,
+  TextPath,
+  Path,
+  Polygon,
+  Polyline,
+  Line,
+  Rect,
+  Use,
+  Image,
+  Symbol,
+  Defs,
+  LinearGradient,
+  RadialGradient,
+  Stop,
+  ClipPath,
+  Pattern,
+  Mask,
+} from 'react-native-svg';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+/* Use this if you are using Expo
+import { Svg } from 'expo';
+const { Circle, Rect } = Svg;
+*/
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import React from 'react';
+import {View, StyleSheet, Dimensions} from 'react-native';
 
-type Props = {};
-export default class App extends Component<Props> {
+// Percentages work in plain react-native but aren't supported in Expo yet, workaround with this or onLayout
+const {width, height} = Dimensions.get('window');
+
+export default class SvgExample extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {
+        line: [10, 20, 30],
+      }
+    }
+  }
+
   render() {
+    const {data} = this.state;
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View style={{flexDirection: 'row', paddingTop: 80, borderWidth: 2}}>
+        <Svg height={height * 0.5} width={width} viewBox={`20 20 100 100`}>
+          <Line
+            x1="60"
+            y1="0"
+            x2="62"
+            y2="100"
+            stroke="rgb(122,178,233)"
+          />
+          <Line
+            x1="20"
+            y1="60"
+            x2="100"
+            y2="60"
+            stroke="rgb(122,178,233)"
+          />
+          <Line
+            x1="30"
+            y1="100"
+            x2="90"
+            y2="25"
+            stroke="rgb(122,178,233)"
+          />
+          <Polygon
+            points="40 90,65 60, 30 60"
+            fill="red"
+          />
+          <Polygon
+            points="85 30, 60 60, 95 60"
+            fill="lime"
+          />
+        </Svg>
       </View>
-    );
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import pieChart from './src/pieChart';
 import CircleExample from './src/circleExample';
-import StackedBarChart from './src/stackedBarChart';
+//import StackedBarChart from './src/stackedBarChart';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => StackedBarChart);
+AppRegistry.registerComponent(appName, () => App);
